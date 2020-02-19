8c2202f3a93bdede71f0251cb66afe9681b396bf
Manoj Bhat <bhatanm@gmail.com>
2020-02-19 17:50:23 +0530

    Fixed post modification button accessibility

--- src/components/Post.js
+++ src/components/Post.js
@@ -14,22 +14,16 @@ const Post = ({
     <div className='post-container'>
       <h1>{head}</h1>
       <p>{body}</p>
-      <div className='post-ctrl'>
-        <button
-          onClick={() => onHandleEdit(postId)}
-          style={{ display: uid !== currentUser.uid ? "none" : "inline-block" }}
-          className='edit-btn'
-        >
-          Edit
-        </button>
-        <button
-          onClick={() => onHandleDelete(postId)}
-          style={{ display: uid !== currentUser.uid ? "none" : "inline-block" }}
-          className='delete-btn'
-        >
-          Delete
-        </button>
-      </div>
+      {currentUser.uid === uid ? (
+        <div className='post-ctrl'>
+          <button onClick={() => onHandleEdit(postId)} className='edit-btn'>
+            Edit
+          </button>
+          <button onClick={() => onHandleDelete(postId)} className='delete-btn'>
+            Delete
+          </button>
+        </div>
+      ) : null}
     </div>
   );
 };
